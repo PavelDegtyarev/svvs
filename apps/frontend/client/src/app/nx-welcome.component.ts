7f4eb33f6a56e7899f6252194bdd5763014a85ba
PavelDegtyarev <pavel.ural@mail.ru>
2024-12-16 12:14:37 +0500

    feat(IDE): change setting int* reformat
    * change setting IDE
    * reformat code
    
    #2

--- apps/frontend/client/src/app/nx-welcome.component.ts
+++ apps/frontend/client/src/app/nx-welcome.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, ViewEncapsulation } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-nx-welcome',
@@ -773,11 +773,11 @@ import { CommonModule } from '@angular/common';
               Build, test and lint your app
             </summary>
             <pre><span># Build</span>
-nx build 
+nx build
 <span># Test</span>
-nx test 
+nx test
 <span># Lint</span>
-nx lint 
+nx lint
 <span># Run them together!</span>
 nx run-many -t build test lint</pre>
           </details>
@@ -866,4 +866,5 @@ nx g &#64;nx/angular:component ui/src/lib/button</pre>
   styles: [],
   encapsulation: ViewEncapsulation.None,
 })
-export class NxWelcomeComponent {}
+export class NxWelcomeComponent {
+}
